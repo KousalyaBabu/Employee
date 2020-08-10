8e926b4e836f80fc65b166e105ac2105baa1e7d3
Kousalya <kousalya551k@gmail.com>
2020-08-10 13:36:14 +0530

    Employee MVVM

--- app/src/main/java/com/example/employeeinfoapplication/EmployeeActivity.kt
+++ app/src/main/java/com/example/employeeinfoapplication/EmployeeActivity.kt
@@ -1,10 +1,10 @@
 package com.example.employeeinfoapplication
 
 import android.os.Bundle
-import android.view.Gravity
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.Menu
 import android.view.MenuItem
-import android.view.View
 import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
@@ -12,11 +12,11 @@ import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.employeeinfoapplication.ui.EmployeeAdapter
-import com.example.employeeinfoapplication.ui.EmployeeLayout
 import com.example.employeeinfoapplication.ui.EmployeeViewModel
 
-class EmployeeActivity:AppCompatActivity() {
+class EmployeeActivity:AppCompatActivity(),TextWatcher {
     private var employeeViewModel: EmployeeViewModel?=null
+    private var recyclerView:RecyclerView?=null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,56 +28,31 @@ class EmployeeActivity:AppCompatActivity() {
         linearLayout.addView(searchLayout)
         val searchEditText= EditText(this)
         searchLayout.addView(searchEditText)
+        searchEditText.hint="Search"
+        searchEditText.addTextChangedListener(this)
         searchEditText.layoutParams=LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT,LinearLayout.LayoutParams.WRAP_CONTENT,0.5F)
 
-        val searchButton= Button(this)
-        searchButton.text="Search"
-        searchLayout.addView(searchButton)
-
-        val mainLayout=LinearLayout(this).apply {
-            orientation=LinearLayout.VERTICAL
-            gravity=Gravity.END
-            visibility=View.GONE
-        }
-        linearLayout.addView(mainLayout)
-
-        val closeImageView=ImageView(this).apply {
-            setImageResource(R.drawable.close)
-            layoutParams=LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT,LinearLayout.LayoutParams.WRAP_CONTENT)
-        }
-        mainLayout.addView(closeImageView)
-
-        val employeeLayout=
-            EmployeeLayout(this)
-        mainLayout.addView(employeeLayout)
-
-        val recyclerView = RecyclerView(this)
+        recyclerView = RecyclerView(this)
         linearLayout.addView(recyclerView)
-        recyclerView.layoutManager = LinearLayoutManager(this)
-
-        searchButton.setOnClickListener {
-            if(searchEditText.text.toString().isNotEmpty()) {
-                val employee=employeeViewModel?.searchByName(searchEditText.text.toString())
-                employeeLayout.setEmployee(employee!!)
-                recyclerView.visibility=View.GONE
-                mainLayout.visibility=View.VISIBLE
-            }else{
-                mainLayout.visibility=View.GONE
-                recyclerView.visibility=View.VISIBLE
-            }
-        }
-
-        closeImageView.setOnClickListener {
-            mainLayout.visibility=View.GONE
-            recyclerView.visibility=View.VISIBLE
-        }
+        recyclerView?.layoutManager = LinearLayoutManager(this)
 
         employeeViewModel=ViewModelProviders.of(this)[EmployeeViewModel::class.java]
         employeeViewModel?.init(this)?.observe(this,(Observer {
-            recyclerView.adapter =
-                EmployeeAdapter(it)
-            recyclerView.adapter?.notifyDataSetChanged()
+            recyclerView?.adapter = EmployeeAdapter(it)
+            recyclerView?.adapter?.notifyDataSetChanged()
         }))
+    }
+
+    override fun afterTextChanged(s: Editable?) {
+    }
+
+    override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+    }
+
+    override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+        val employees=employeeViewModel?.init(this)?.value
+        val list=employees?.filter { it.employeeName.contains(s!!,true) }
+        recyclerView?.adapter=EmployeeAdapter(list!!)
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
